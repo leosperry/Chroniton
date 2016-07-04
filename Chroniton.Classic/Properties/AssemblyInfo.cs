7bedeeb06f9af4848432991539ba73fa90d201e4
leosperry <leonard.sperry@live.com>
2016-07-04 00:42:33 -0700

    updating nuget info

--- Chroniton.Classic/Properties/AssemblyInfo.cs
+++ Chroniton.Classic/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Chroniton")]
-[assembly: AssemblyDescription("A library for running tasks on schedules.")]
+[assembly: AssemblyDescription("A library for running tasks(jobs) on schedules.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Leonard Sperry")]
 [assembly: AssemblyProduct("Chroniton")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]

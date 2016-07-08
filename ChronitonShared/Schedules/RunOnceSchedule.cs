aa5fdf95d9d9eda05d6afb538a9fbe1efab957ba
leosperry <leonard.sperry@live.com>
2016-07-08 12:46:06 -0700

    Adding job expiration support

---
+++ ChronitonShared/Schedules/RunOnceSchedule.cs
@@ -0,0 +1,50 @@
+﻿using Chroniton;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chroniton.Schedules
+{
+    public class RunOnceSchedule : ISchedule
+    {
+        DateTime? _runAt = null;
+
+        public string Name { get; set; }
+
+        public DateTime NextScheduledTime(IScheduledJob scheduledJob)
+        {
+            if (scheduledJob.RunCount > 0)
+            {
+                return Chroniton.Constants.Never;
+            }
+            else
+            {
+                return _runAt.HasValue ? _runAt.Value : DateTime.UtcNow;
+            }
+        }
+
+        /// <summary>
+        /// when NextScheduledTime is called, will return DateTime.UtcNow
+        /// </summary>
+        public RunOnceSchedule()
+        {
+
+        }
+
+        /// <summary>
+        /// when NextScheduledTime is called, will return runAt
+        /// </summary>
+        public RunOnceSchedule(DateTime runAt)
+        {
+            _runAt = runAt;
+        }
+
+        /// <summary>
+        /// when NextScheduledTime is called, will return DateTime.UtcNow + runIn
+        /// </summary>
+        public RunOnceSchedule(TimeSpan runIn)
+        {
+            _runAt = DateTime.UtcNow.Add(runIn);
+        }
+    }
+}

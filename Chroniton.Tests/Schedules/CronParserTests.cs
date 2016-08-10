62780b01ed88194403d47552853b9f592c53ab58
leosperry <leonard.sperry@live.com>
2016-08-10 15:31:39 -0700

    Updating version strings

--- Chroniton.Tests/Schedules/CronParserTests.cs
+++ Chroniton.Tests/Schedules/CronParserTests.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Chroniton.Schedules.Cron;
+using System;
 using Xunit;
-using Chroniton.Schedules.Cron;
 
 namespace Chroniton.Tests.Schedules
 {
